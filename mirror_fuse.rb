7878f37076312e31954022fea6562c59f04c8ded
Aleksei <aleksei.kornev@gmail.com>
2011-09-14 00:44:23 +0400

    Add raw methods, add executable? method, rename check method

--- mirror_fuse.rb
+++ mirror_fuse.rb
@@ -11,6 +11,7 @@ class MirrorFuse
 
     def initialize(root)
         @root = root
+        @files = {}
     end
 
     def contents(path)
@@ -26,6 +27,12 @@ class MirrorFuse
         File.file? "#{@root}#{path}"
     end
 
+    def executable?(path)
+        result = File.executable?("#{@root}#{path}")
+        $LOGGER.debug("Is #{path} executable = #{result}")
+        return result
+    end
+
     def read_file(path)
         $LOGGER.debug("Read file by #{path}")
         IO.read("#{@root}#{path}")
@@ -33,7 +40,7 @@ class MirrorFuse
 
     # Write to a file
     def can_write?(path)
-        result = can_write(path)
+        result = write?(path)
         $LOGGER.debug("Can write #{path} = #{result}")
         return result
     end
@@ -47,7 +54,7 @@ class MirrorFuse
 
     # Delete a file
     def can_delete?(path)
-        result = can_write(path)
+        result = write?(path)
         $LOGGER.debug("Can delete file path : #{path} = #{result}")
         return result
     end
@@ -59,7 +66,7 @@ class MirrorFuse
 
     # Make a new directory
     def can_mkdir?(path)
-        result = can_write(path)
+        result = write?(path)
         $LOGGER.debug("Can mkdir path : #{path} = #{result}")
         return result
     end
@@ -74,7 +81,7 @@ class MirrorFuse
 
     # Delete an existing directory.
     def can_rmdir?(path)
-        result = can_write(path)
+        result = write?(path)
         $LOGGER.debug("Can rmdir path : #{path} = #{result}")
         return result
     end
@@ -83,9 +90,62 @@ class MirrorFuse
         $LOGGER.debug("Rmdir path: #{path}")
         Dir.rmdir("#{@root}#{path}")
     end
+
+    def size(path)
+        size = File.size(File.join(@root, path))
+        $LOGGER.debug("Size of file #{path} size = #{size}")
+        return size
+    end
+
+    def raw_open(path, mode)
+            $LOGGER.debug("Open file path = #{path}, mode = #{mode}")
+            return true if @files.has_key?(path)
+            @files[path] = File.open(File.join(@root, path), mode)
+            return true
+        rescue
+            $LOGGER.error("Failed to open file #{path}")
+            return false
+    end
+
+    def raw_read(path, off, size)
+            $LOGGER.debug("Raw read file #{path}")
+            file = @files[path]
+            return unless file
+            $LOGGER.debug("File is opened raw read path = #{path}, 
+                            off = #{off}, size = #{size}")
+            file.seek(off, File::SEEK_SET)
+            file.read(size)
+        rescue
+            $LOGGER.error("Failed to raw read file path = #{path}, 
+                            off = #{off}, size = #{size}")
+            return nil
+    end
+
+    def raw_write(path, off, sz, buf)
+            $LOGGER.debug("Raw write to file #{path}")
+            file = @files[path]
+            return unless file
+            $LOGGER.debug("File is opened raw write to file path=#{path}, 
+                            off = #{off}, size = #{size}, buf = #{buf}")
+            file.seek(off, File::SEEK_SET)
+            file.write(buf[0, sz])
+        rescue
+            $LOGGER.error("Failed to raw write file path = #{path}, 
+                            off = #{off}, size = #{size}, buf = #{buf}")
+    end
+
+    def raw_close(path)
+            $LOGGER.debug("Close file #{path}")
+            file = @files[path]
+            return unless file
+            file.close
+            @files.delete path
+        rescue
+            $LOGGER.error("Failed to close file #{path}")
+    end
     
     private
-    def can_write(path)
+    def write?(path)
         real_path = Pathname.new("#{@root}#{path}")
         File.writable?(real_path.parent)
     end
